86d4f4d42cf4bb5331dc27c9fea9490ce900c64b
martinavagyan <martinavagyan@gmail.com>
2017-05-22 09:01:49 +0200

    travis set up test

---
+++ test/sum.spec.js
@@ -0,0 +1,8 @@
+import {expect} from 'chai';
+import sum from '../src/sum.js';
+describe('src/sum.js', _ => {
+  it('should add 1 + 1 to make two', () => {
+    let result = sum(1,1);
+    expect(result).to.equal(2);
+  })
+})
